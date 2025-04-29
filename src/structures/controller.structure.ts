f413b88454b16bb372d35da1ec1f5ba43caf689f
LinCie <aldiofernanda@protonmail.com>
2025-04-29 16:59:30 +0700

    feat: adjust controller structure

--- src/structures/controller.structure.ts
+++ src/structures/controller.structure.ts
@@ -1,30 +1,40 @@
 import express from "express"
-import type { Request, Response, NextFunction } from "express"
+import type { RequestHandler } from "express"
 import type { ZodTypeAny } from "zod"
 import { validatorMiddleware } from "@/middlewares/validator.middleware"
 
 interface RouteDefinition {
   method: "get" | "post" | "put" | "delete" | "patch"
   path: string
-  handler: (req: Request, res: Response, next: NextFunction) => void
+  handler: RequestHandler
   schema?: ZodTypeAny
+  middlewares?: RequestHandler[]
 }
 
 abstract class Controller {
   public readonly router = express.Router()
 
   protected bindRoutes(routes: RouteDefinition[]): void {
-    routes.forEach((route) => {
-      if (route.schema) {
-        this.router[route.method](
-          route.path,
-          validatorMiddleware(route.schema),
-          route.handler.bind(this)
-        )
-      } else {
-        this.router[route.method](route.path, route.handler.bind(this))
-      }
+    routes.forEach(({ method, path, handler, middlewares, schema }) => {
+      // Middlewares
+      const mws = (middlewares ?? []).map((mw) =>
+        this.asyncHandler(mw.bind(this))
+      )
+
+      // Validators
+      const validators = schema ? [validatorMiddleware(schema)] : []
+
+      // Handler
+      const finalHandler = this.asyncHandler(handler.bind(this))
+
+      this.router[method](path, ...mws, ...validators, finalHandler)
     })
+  }
+
+  private asyncHandler(fn: RequestHandler): RequestHandler {
+    return (req, res, next) => {
+      Promise.resolve(fn(req, res, next)).catch(next)
+    }
   }
 }
 
